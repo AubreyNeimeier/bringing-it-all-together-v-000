67996e1fbba0d4f39468dc6dfc016ed978988871
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-06 19:37:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -75,9 +75,9 @@ attr_accessor :name, :breed, :id
       new_dog = Dog.new(new_dog_hash)
     end
 
-    def self.find_or_create_by(name, breed)
+    def self.find_or_create_by(attr_hash)
       sql = ("SELECT * FROM dogs WHERE name = ? AND breed = ?")
-      result = DB[:conn].execute(sql, name, breed)
+      result = DB[:conn].execute(sql, attr_hash[1], attr_hash[2])
       if !result.empty?
         new_dog_hash = {id: result[0], name: result[1], breed: result[2]}
         new_dog_obj = Dog.new(new_dog_hash)
